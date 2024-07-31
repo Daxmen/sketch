c1f883a771898aaf7eae612b67e2e2048f4a0b92
Daxmen <andrerojasrios@gmail.com>
2024-07-31 07:08:01 -0500

    se agrego el js del sketch

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 const container = document.querySelector('#container');
-
 container.style.cssText = 
-'display:flex;flex-wrap:wrap;flex-grow:1;border:solid 1px red;width:800px;text-align:center'
+'display:flex;flex-wrap:wrap;border:solid 1px red;width:800px;height:800px;margin:0 auto';
 
 
 for (let i = 0; i<256;i++){
     const div = document.createElement('div');
-    div.style.cssText = "border:1px black solid;"
-    div.textContent = i
+    div.style.cssText = "display:flex;outline:1px blue solid;width:50px;height:50px;margin:0px";
+    // div.textContent = i;
     container.appendChild(div);
 }
