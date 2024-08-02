7301ccf87aeac574b78dff8766d70ca1aca64dfe
Daxmen <andrerojasrios@gmail.com>
2024-08-02 18:29:52 -0500

    se agrego mas codigo

--- sketch.js
+++ sketch.js
@@ -1,11 +1,59 @@
 const container = document.querySelector('#container');
+// const containertwo = document.createElement("div");
 container.style.cssText = 
-'display:flex;flex-wrap:wrap;border:solid 1px red;width:800px;height:800px;margin:0 auto';
+'display:flex;flex-wrap:wrap;border:solid 1px red;width:960px;height:960px;margin:0 auto';
+// containertwo.style.cssText = 
+// 'display:flex;flex-wrap:wrap;width:960px;height:960px;margin:0 auto';
+const buttonChange = document.querySelector("#canvaslen");
+buttonChange.style.height = "50px";
+buttonChange.style.width = "150px";
+buttonChange.textContent = "Changue Canvas";
+buttonChange.style.margin = "5px"
+
+// container.appendChild(containertwo);
+
+buttonChange.addEventListener("click",()=>{
+    valueCanvas = prompt('Canvas size');
+    canvasSize(valueCanvas);
+    chgbackground();
 
 
-for (let i = 0; i<256;i++){
-    const div = document.createElement('div');
-    div.style.cssText = "display:flex;outline:1px blue solid;width:50px;height:50px;margin:0px";
-    // div.textContent = i;
-    container.appendChild(div);
+
+})
+
+
+function canvasSize(a) {
+    const containertwo = document.createElement("div");
+    containertwo.style.cssText = 
+    'display:flex;flex-wrap:wrap;width:960px;height:960px;margin:0 auto';
+    container.appendChild(containertwo);
+
+    for (let i = 0; i<a*a;i++){
+        const div = document.createElement('div');
+        div.style.cssText = "display:flex;outline:1px blue solid;margin:0px";
+        var b = 960/a;
+        // console.log(b);
+        div.style.width = String(b)+"px";
+        div.style.height = String(b)+"px";
+        // div.textContent = i;
+        div.classList.add("pixel");
+        containertwo.appendChild(div);
+    }
 }
+
+function chgbackground() {
+    const pxl = document.querySelectorAll(".pixel");
+    pxl.forEach((pixel)=>{
+        pixel.addEventListener('mouseover',()=>{
+            pixel.style.backgroundColor="red";
+
+        });
+    });
+};
+
+// pxl.setAttribute("style","background-color:blue;");
+
+// pixel.addEventListener('mouseover', ()=>{
+//     pixel.setAttribute("style","background-color:blue;");
+// }
+// );
